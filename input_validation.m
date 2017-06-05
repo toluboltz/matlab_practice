343cda6e6230b5d537fc89ddf904326cf4a5d4be
Tolulope Akinbulire <toluboltz@gmail.com>
2017-06-05 16:23:18 -0400

    Changed file name from 'practice' to 'input_validation'

---
+++ input_validation.m
@@ -0,0 +1,20 @@
+prompt1= 'What is side a?';
+prompt2= 'What is side b?';
+
+a=input(prompt1,'s');
+a= str2double(a);
+
+if isnan(a)
+    disp('Please enter a number');
+    return
+end
+
+b=input(prompt2,'s');
+b=str2double(b);
+
+if isnan(b) 
+    disp('Please enter a number');
+    return
+end
+
+c = sqrt(a^2+b^2)
