9b0137fdedefc68d3d0b8b0cf66a703d4e8080aa
Tolulope Akinbulire <toluboltz@gmail.com>
2017-06-05 00:46:54 -0400

    Solution to input validation

--- practice.m
+++ practice.m
@@ -2,9 +2,16 @@ close all;
 clear;
 
 prompt = 'Enter the value of a: ';
-a = input(prompt);
-if isnumeric(a) == 0
-    disp('wrong');
+a = input(prompt, 's');
+a = str2double(a);
+% I noticed that doing the str2double function will return NaN (not a
+% number). So what I did here in the 'if' statement is to check if it
+% returned NaN and then told it to display a message and exit the program
+% if it does.
+if isnan(a)
+    disp('wrong input')
+    return
 end
+
 % else continue with the process below
 disp('continue');
