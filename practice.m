b43e7693daf33ff8e5cc62c256336dfee4c69d26
esther-akinwumi <funmie@rocketmail.com>
2017-06-05 01:09:55 -0400

    Update practice.m

--- practice.m
+++ practice.m
@@ -1,23 +1,17 @@
-close all;
-clear;
-
-prompt = 'Enter the value of a: ';
-a = input(prompt);
-if isnumeric(a) == 0
-    disp('wrong');
-end
-% else continue with the process below
-disp('continue');
-
-%esther
 prompt1= 'What is side a?'
 prompt2= 'What is side b?'
-a=input(prompt1)
-if isnumeric(a)==0
+a=input(prompt1,'s')
+a= str2double(a)
+check=isnumeric(a)
+if isnan(a)
     disp('Please enter a number')
+    return
 end
-b=input(prompt2)
-if isnumeric(b)==0 
+b=input(prompt2,'s')
+b=str2double(b)
+if isnan(b) 
     disp('Please enter a number')
+    return
 end
     c=sqrt(a^2+b^2)
+ 
